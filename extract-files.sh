LineageOS/android_device_samsung_serrano-common

misc: Update proprietary extract script
Change-Id: I36df8d03313b5d76464ff47787d668d7c656e09a

extract-files.sh
@@ -1,39 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-export VENDOR=samsung
-export DEVICE=serrano-common
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+        DIR=`dirname $FILE`
+        if [ ! -d $2/$DIR ]; then
+            mkdir -p $2/$DIR
+        fi
+        # Try CM target first
+        adb pull /system/$DEST $2/$DEST
+        # if file does not exist try OEM target
+        if [ "$?" != "0" ]; then
+            adb pull /system/$FILE $2/$DEST
+        fi
+    done
+}
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+BASE=../../../vendor/samsung/serrano-common/proprietary
 rm -rf $BASE/*
-for FILE in `egrep -v '(^#|^$)' ../$DEVICE/proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$FILE
-  else
-    cp $SRC/system/$FILE $BASE/$FILE
-  fi
-done
 
-./../$DEVICE/setup-makefiles.sh
+DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $DEVBASE/*
+
+extract ../../samsung/serrano-common/proprietary-files.txt $DEVBASE
+
+./../../samsung/serrano-common/setup-makefiles.sh

phubarr/android_device_samsung_melius-common

extract-files.sh
@@ -1,39 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-export VENDOR=samsung
-export DEVICE=melius-common
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+        DIR=`dirname $FILE`
+        if [ ! -d $2/$DIR ]; then
+            mkdir -p $2/$DIR
+        fi
+        # Try CM target first
+        adb pull /system/$DEST $2/$DEST
+        # if file does not exist try OEM target
+        if [ "$?" != "0" ]; then
+            adb pull /system/$FILE $2/$DEST
+        fi
+    done
+}
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+BASE=../../../vendor/samsung/melius-common/proprietary
 rm -rf $BASE/*
-for FILE in `egrep -v '(^#|^$)' ../$DEVICE/proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$FILE
-  else
-    cp $SRC/system/$FILE $BASE/$FILE
-  fi
-done
 
-./../$DEVICE/setup-makefiles.sh
+DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $DEVBASE/*
+
+extract ../../samsung/melius-common/proprietary-files.txt $DEVBASE
+
+./../../samsung/melius-common/setup-makefiles.sh